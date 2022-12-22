48dd0e5e41b3a6cbc31179b220b65f8b4817b94b
krunalgiri.gosai <krunalgiri.gosai@trigyn.com>
2022-12-22 19:45:08 +0530

    completed till 1.10

---
+++ part1/unicafe/src/StatisticLine.js
@@ -0,0 +1,8 @@
+function StatisticsLine(props) {
+
+    return (
+        <p><b>{props.name} :</b> {props.value}</p>
+    )
+}
+
+export default StatisticsLine;
--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -1,3 +1,5 @@
+import StatisticsLine from "./StatisticLine"
+
 function Statistics(props) {
 
     return (
@@ -6,12 +8,12 @@ function Statistics(props) {
             {(props.good == 0 && props.neutral == 0 && props.bad == 0)
                 ? (<p>No feedback available, Please give your feedback</p>)
                 : (<>
-                    <p><b>Good :</b> {props.good}</p>
-                    <p><b>Neutral :</b> {props.neutral}</p>
-                    <p><b>Bad :</b> {props.bad}</p>
-                    <p><b>All :</b> {props.good + props.neutral + props.bad}</p>
-                    <p><b>Average :</b> {(props.good - props.bad) / (props.good + props.neutral + props.bad)}</p>
-                    <p><b>Positive :</b> {(props.good * 100) / (props.good + props.neutral + props.bad)} %</p>
+                    <StatisticsLine name="Good" value={props.good}></StatisticsLine>
+                    <StatisticsLine name="Neutral" value={props.neutral}></StatisticsLine>
+                    <StatisticsLine name="Bad" value={props.bad}></StatisticsLine>
+                    <StatisticsLine name="All" value={props.good + props.neutral + props.bad}></StatisticsLine>
+                    <StatisticsLine name="Average" value={(props.good - props.bad) / (props.good + props.neutral + props.bad)}></StatisticsLine>
+                    <StatisticsLine name="Positive" value={((props.good * 100) / (props.good + props.neutral + props.bad)) + ' %'}></StatisticsLine>
                 </>
                 )}
 
