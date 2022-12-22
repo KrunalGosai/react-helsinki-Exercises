56e46c16c08d1e5262a39dfde6639de85ac4f882
krunalgiri.gosai <krunalgiri.gosai@trigyn.com>
2022-12-22 14:10:17 +0530

    completed 1.2

--- part1/courseinfo/src/Content.js
+++ part1/courseinfo/src/Content.js
@@ -1,15 +1,11 @@
+import Part from "./Part";
+
 function Content(props) {
     return (
         <>
-            <p>
-                {props.content.part1} {props.content.exercises1}
-            </p>
-            <p>
-                {props.content.part2} {props.content.exercises2}
-            </p>
-            <p>
-                {props.content.part3} {props.content.exercises3}
-            </p>
+            <Part part={props.content.part1} exercises={props.content.exercises1}></Part>
+            <Part part={props.content.part2} exercises={props.content.exercises2}></Part>
+            <Part part={props.content.part3} exercises={props.content.exercises3}></Part>
         </>
     );
 }
