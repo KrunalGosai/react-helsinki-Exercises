2b8e17f9432d15e43c51b88cdcfefbeb458c0cb4
krunalgiri.gosai <krunalgiri.gosai@trigyn.com>
2022-12-22 19:23:10 +0530

    completed 1.7

--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -1,16 +1,17 @@
 function Statistics(props) {
 
-    let noData = props.good == 0 && props.neutral == 0 && props.bad == 0
-
     return (
         <>
             <h2>Statistics</h2>
             {(props.good == 0 && props.neutral == 0 && props.bad == 0)
                 ? (<p>No feedback available, Please give your feedback</p>)
                 : (<>
-                    <p>Good : {props.good}</p>
-                    <p>Neutral : {props.neutral}</p>
-                    <p>Bad : {props.bad}</p>
+                    <p><b>Good :</b> {props.good}</p>
+                    <p><b>Neutral :</b> {props.neutral}</p>
+                    <p><b>Bad :</b> {props.bad}</p>
+                    <p><b>All :</b> {props.good + props.neutral + props.bad}</p>
+                    <p><b>Average :</b> {(props.good - props.bad) / (props.good + props.neutral + props.bad)}</p>
+                    <p><b>Positive :</b> {(props.good * 100) / (props.good + props.neutral + props.bad)} %</p>
                 </>
                 )}
 
